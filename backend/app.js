3b2c6df0f4a1d6f278b1f59b3678c067e2d4cb51
JerryMur1 <eeests1@yandex.ru>
2021-04-10 19:37:49 +0300

    full

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,14 @@ const { errors } = require('celebrate');
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const app = express();
 const { PORT = 3001 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+const app = express();
+
 const auth = require('./middlewares/auth');
 
 const options = {
@@ -25,8 +26,9 @@ const options = {
   ],
   credentials: true,
 };
-
-app.use('*', cors(options));
+app.use(requestLogger);
+app.use(bodyParser.json());
+app.use(cors());
 
 const cardsRouter = require('./routes/cards.js');
 const usersRouter = require('./routes/users.js');
@@ -34,8 +36,6 @@ const signIn = require('./routes/users.js');
 const signUp = require('./routes/users.js');
 const NotFoundError = require('./errors/error');
 
-app.use(requestLogger);
-app.use(bodyParser.json());
 app.use('/', signIn);
 app.use('/', signUp);
 
@@ -43,7 +43,7 @@ app.use(auth);
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
 app.use('*', (req, res) => {
-  throw new NotFoundError();
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
 app.use(errorLogger);
 app.use(errors());
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -115,7 +115,7 @@ signup(body) {
 }
 
 const api = new Api({
-  baseUrl: "//api.borman.nomoredomains.club/",
+  baseUrl: "https://api.borman.nomoredomains.club/",
  
 });
 
